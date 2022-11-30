20fd9b5102d5624422f354c63c3209bcd8d6edd1
Keny <d8956303@gmail.com>
2022-11-30 16:26:49 +0800

    完成驗證碼的開發 #4

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string  recapta { get; set; }
 
         //這裡是用線上編輯更新的
         
